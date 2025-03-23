816bfed3dcb6135091a65a03a03309c62f917372
Aniket793 <vinitkshirsagar18@gmail.com>
2025-03-23 22:53:50 +0530

    minor changes.

--- App.js
+++ App.js
@@ -1,7 +1,7 @@
 import React, { useEffect } from 'react';  // ✅ Import useEffect
 import { View, Text } from 'react-native';
 import MainNavigator from './src/navigation/MainNavigator';
-import { supabase } from './src/lib/supabase';
+import { supabase } from './.env/supabase.js';
 
 export default function App() {
     return (
